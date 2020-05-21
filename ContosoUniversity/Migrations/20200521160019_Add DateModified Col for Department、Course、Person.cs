3e855334c4372314828df7a8bf368277d238228b
Randx <wind04181986@gmail.com>
2020-05-22 00:07:20 +0800

    Init

---
+++ ContosoUniversity/Migrations/20200521160019_Add DateModified Col for Department、Course、Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class AddDateModifiedColforDepartmentCoursePerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Person",
+                type: "datetime",
+                nullable: false,
+                defaultValue: "(GetDate())");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Department",
+                type: "datetime",
+                nullable: false,
+                defaultValue: "(GetDate())");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Course",
+                type: "datetime",
+                nullable: false,
+                defaultValue: "(GetDate())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Course");
+        }
+    }
+}
